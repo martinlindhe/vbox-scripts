920498e3391f904ea1e82892284792fc87f92edf
Martin Lindhe <martin.lindhe@freespee.com>
2014-06-11 15:05:42 +0200

    added backup script

---
+++ TODO.md
@@ -0,0 +1,5 @@
+## TODO
+
+* add setting file, containing list of names/uuid of a group of vm:s
+* update scripts to take 1 parameter, setting file
+
---
+++ compress-vms.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# compresses each directory in /Users/username/VirtualBox VMs/ into its own 7z archive
+# NOTE: tested on OSX Mavericks, should work on linux
+
+
+# TODO iterate over directories in vmDir, excluding backupDir
+
+# NOTE: 20s with compression 0 (store)
+
+compression=0
+
+baseDir=`pwd`
+timeStamp=`date +"%Y%m%d_%H%M"`
+
+vmDir=$HOME"/VirtualBox VMs"
+backupDir=$vmDir/backups
+
+
+mkdir "$backupDir"
+
+cd "$vmDir"
+
+7z a -mx=$compression -xr@$baseDir/exclude-from-backup.txt "$backupDir/debian-5-$timeStamp-compression-$compression.7z" "Debian 5 (Lenny) server 64bit"
+7z a -mx=$compression -xr@$baseDir/exclude-from-backup.txt "$backupDir/debian-6-$timeStamp-compression-$compression.7z" "Debian 6 (Squeeze) server 64bit"
+7z a -mx=$compression -xr@$baseDir/exclude-from-backup.txt "$backupDir/debian-7-$timeStamp-compression-$compression.7z" "Debian 7 (Wheezy) server 64bit"
+7z a -mx=$compression -xr@$baseDir/exclude-from-backup.txt "$backupDir/debian-sid-$timeStamp-compression-$compression.7z" "Debian Unstable (sid) server 64bit"
+
+cd "$baseDir"
